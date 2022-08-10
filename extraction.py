e697be540449f1be88aa13c816352d893cbd0a6c
flaviasipres <flaviasipres@poli.ufrj.br>
2022-08-09 21:20:04 -0300

    set website url param

--- extraction.py
+++ extraction.py
@@ -3,3 +3,8 @@ import pandas as pd
 from bs4 import BeautifulSoup as bs
 import requests
 
+#%%
+
+baseUrl = "https://www.vivareal.com.br/"
+url = f"{baseUrl}venda/parana/curitiba/?pagina=" + "{}"
+
